d4f1cbe53341e85f70501f24250f1941b88fedae
Marcin Florek <florekdeveloper@gmail.com>
2023-01-13 17:16:36 +0100

    small changes in css

--- src/Section/index.js
+++ src/Section/index.js
@@ -6,9 +6,8 @@ const Section = ({ title, body, extraHeaderContent }) => (
             <h2 className="section__title">{title}</h2>
             {extraHeaderContent}
         </header>
-        <div className="section__body">
-            {body}
-        </div>
+        {body}
+        
     </section >
 );
 
