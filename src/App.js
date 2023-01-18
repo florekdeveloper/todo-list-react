a58327c1f46eed5fd4fb1119f3bebb470961fcbf
Marcin Florek <florekdeveloper@gmail.com>
2023-01-18 17:36:36 +0100

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ console.log(name);
 function App() {
   const [hideDone, setHideDone] = useState(false);
   const [tasks, setTasks] = useState([
-    { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zjeść obiad", done: true },
   ]);
 
   const toggleHideDone = () => {
