5a5891f2f3fec9e6b022a28773bbc21d96a990ca
Marcin Florek <florekdeveloper@gmail.com>
2023-01-26 11:58:12 +0100

    styled-components added

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,6 +1,8 @@
-.container {
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
     background-color: #eee;
     margin: auto;
     padding: 10px;
     max-width: 700px;
-} 
+`;
