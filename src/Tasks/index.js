42f7b21d5633cf3e72481a15ac1bbc4b06d89e09
Marcin Florek <florekdeveloper@gmail.com>
2023-01-26 10:13:46 +0100

    styled component added

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,34 +1,29 @@
-import "./style.css";
+import { List, Item, Content, Button } from "./styled";
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <ul className="tasks">
+    <List>
         {tasks.map((task) => (
-            <li key={task.id}
-                className={`tasks__item ${task.done && hideDone
-                    ? "tasks__item--hidden"
-                    : ""}`
-                }
+            <Item 
+                key={task.id}
+                hidden={task.done && hideDone}
             >
-                <button
-                    className="tasks__button tasks__button--toggleDone"
+                <Button
+                    toggleDone
                     onClick={() => toggleTaskDone(task.id)}
                 >
                     {task.done ? "✓" : ""}
-                </button>
-                <span
-                    className={
-                        `tasks__content${task.done ? " tasks__content--done" : ""}`
-                    }>
-                    {task.content}
-                </span>
-                <button
-                    className="tasks__button tasks__button--remove"
+                </Button>
+                <Content done={task.done}>
+                  {task.content}
+                </Content>
+                <Button
+                    remove
                     onClick={() => removeTask(task.id)}
                 >
-                    🗑</button>
-            </li>
+                    🗑</Button>
+            </Item>
         ))}
-    </ul>
+    </List>
 );
 
 export default Tasks;
---
+++ src/Tasks/styled.js
@@ -0,0 +1,56 @@
+import styled, { css } from "styled-components";
+
+export const List = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 10px;
+    font-size: large;
+`;
+
+export const Item = styled.li`
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    align-items: center;
+    grid-gap: 10px;
+    border-bottom: 1px solid rgb(245, 235, 235);
+    padding: 10px;
+    padding-left: 0;
+
+    ${({hidden}) => hidden && css`
+        display: none; 
+    `}
+`;
+
+export const Content = styled.span`
+    ${({ done }) => done && css`
+        text-decoration: line-through;
+    `}
+`;
+
+export const Button = styled.button`
+    color: white;
+    border-style: none;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+
+    ${({toggleDone}) => toggleDone && css`
+        color: white;
+        background: green;
+
+        &:hover {
+            cursor: pointer;
+            background: rgb(37, 167, 37);
+        }
+    `}
+
+    ${({remove}) => remove && css`
+        background-color: firebrick;
+        color: white;
+
+        &:hover {
+            cursor: pointer;
+            background-color: rgb(222, 69, 69);
+        }
+    `}
+`;
