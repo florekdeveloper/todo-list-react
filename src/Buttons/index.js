3cacba0a8c6a9a480e703fbc13167791d5bf6a65
Marcin Florek <florekdeveloper@gmail.com>
2023-01-26 11:51:06 +0100

    styled-components added

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,19 +1,19 @@
-import "./style.css";
+import { Wrapper, Button } from "./styled";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     tasks.length > 0 && (
-        <div className="buttons">
-            <button onClick={toggleHideDone} className="buttons__button">
-                {hideDone ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            <button
+        <Wrapper>
+            <Button onClick={toggleHideDone}>
+                {hideDone ? "Pokaż" : "Ukryj"}
+                ukończone
+            </Button>
+            <Button
                 onClick={setAllDone}
-                className="buttons__button"
                 disabled={tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
-            </button>
-        </div>
+            </Button>
+        </Wrapper>
     )
 );
 
---
+++ src/Buttons/styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+export const Button = styled.button`
+    background: transparent;
+    color: #008080;
+    border: none;
+    margin: 0 0 0 20px;
+    transition: color 0.3s;
+
+    @media(max-width: 767px) {
+        flex-basis: 100%;
+        margin: 10px;
+    }
+
+    &:hover {
+        color: #008080dd;
+    }
+
+    &:disadled {
+        color: #ccc;
+    }
+`;
