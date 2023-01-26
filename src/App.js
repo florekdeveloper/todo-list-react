a256b51687c622b7ea2dbce6efc255d22e7163b2
Marcin Florek <florekdeveloper@gmail.com>
2023-01-26 08:49:12 +0100

    App.js code change

--- src/App.js
+++ src/App.js
@@ -13,13 +13,8 @@ console.log(name);
 function App() {
   const [hideDone, setHideDone] = useState(false);
 
-  const tasksFromLocalStorage = localStorage.getItem("tasks");
-
-  const [tasks, setTasks] = useState(
-    tasksFromLocalStorage
-      ? JSON.parse(tasksFromLocalStorage)
-      : []
-  );
+  const [tasks, setTasks] = 
+  useState(JSON.parse(localStorage.getItem("tasks")) || []);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
