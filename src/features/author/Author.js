acd189d4a419f52a8d545a5c6949c28662a1bd4a
Marcin Florek <florekdeveloper@gmail.com>
2023-02-10 19:48:04 +0100

    Author added

--- src/features/author/Author.js
+++ src/features/author/Author.js
@@ -1,6 +1,8 @@
 import Container from "../../common/Container";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
+import { Wrapper, Image, Paragraph } from "./styled";
+import image from "./marcin.jpg";
 
 const Author = () => {
     return (
@@ -9,11 +11,25 @@ const Author = () => {
             <Section
                 title="Marcin Florek"
                 body={
-                  <>nana</>
+                    <Wrapper>
+                        <Image
+                            width={200}
+                            height={200}
+                            src={image}
+                            alt="Zdjecie Marcin"
+                        />
+                        <Paragraph>
+                            <strong>Witam i pozdrawiam wszystkich którzy tutaj zajrzeli 😉</strong><br /><br />
+                                    Mam na imię Marcin i pochodze z Poznania. Wiele lat mieszkałem w Anglii, zajmowałem się wieloma rzeczami
+                                    od inżynierii poprzez zarządzanie i swoją działalność. Programowanie zawsze mnie interesowało ale nigdy nic w tym kierunku nie zrobiłem.
+                                    <br /><br />W zeszłym roku zamieszkałem w Polsce i zapisałem się na kurs programowania w Szkole internetowej <strong>YouCode</strong>.
+                                    <br /><br />Ta aplikacja jest elementem tego kursu, sporo nauki ale satysfakcja gwarantowana 😃
+                        </Paragraph>
+                    </Wrapper>
                 }
             />
         </Container>
-    ); 
+    );
 };
 
 export default Author;
---
+++ src/features/author/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    flex-direction: column;
+  }
+`;
+
+export const Image = styled.img`
+  border-radius: 50%;
+  box-shadow: 5px 5px 10px #aab;
+  padding: 5px;
+`;
+
+export const Paragraph = styled.p`
+  padding: 20px;
+  line-height: 1.5;
+
+`;
